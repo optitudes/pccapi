d27f102094865868fd59530634ed2dbf4ef56d2a
optitudes <sebasacuasssssrio@gmail.com>
2023-07-18 21:47:13 -0500

    fixed link

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -18,7 +18,7 @@ class VideoSeeder extends Seeder
             [
               "title" => "loros que hablan",
               "description" => "Un compilado de tres loros que hablan y son unicos del quindio",
-              "link" => "https://www.youtube.com/watch?v=SCojIEuR9us",
+              "link" => "SCojIEuR9us",
               "banner" => "https://qph.cf2.quoracdn.net/main-qimg-0668e37561c1b1c4aa78d401ea7d1335-lq",
               'project_id' => 1,
               'created_at' => "2022-01-01",
@@ -26,7 +26,7 @@ class VideoSeeder extends Seeder
             [
                 "title" => "loro cantando",
                 "description" => "Un loro endemico del quindio que por su naturaleza puede aprender a cantar",
-                "link" => "https://www.youtube.com/watch?v=amrJc9_iBPQ",
+                "link" => "amrJc9_iBPQ",
                 "banner" => "https://img.point.pet/images/close-up-of-parrot-909966118-5b67b41bc9e77c007bb09fe6.jpg",
                 'project_id' => 1,
               'created_at' => "2022-01-01",
