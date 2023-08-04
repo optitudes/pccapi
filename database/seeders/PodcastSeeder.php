51902715608f0e70ad4a8edfaa17bc38bbd67aaa
optitudes <sebasacuasssssrio@gmail.com>
2023-08-03 22:00:43 -0500

    added test sounds on database

--- database/seeders/PodcastSeeder.php
+++ database/seeders/PodcastSeeder.php
@@ -18,7 +18,7 @@ class PodcastSeeder extends Seeder
             [
               "title" => "historia de los loros que hablan",
               "description" => "Una narración de la bióloga julieta venegas  donde se expone la naturaleza oculta de los loros del quindio",
-              "link" => "https://www.youtube.com/watch?v=_DOVV9EbI2k",
+              "link" => "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3",
               "banner" => "https://i.etsystatic.com/14966180/r/il/4893f6/1259760765/il_1588xN.1259760765_sdn3.jpg",
               'project_id' => 1,
               'created_at' => "2022-01-01",
@@ -26,7 +26,7 @@ class PodcastSeeder extends Seeder
             [
                 "title" => "Acercamiento a la musica y el canto de las aves del quindío",
                 "description" => "Juan sebastian nos expone la relación casi intrinseca entre el canto del loro coroniazul",
-                "link" => "https://www.youtube.com/watch?v=vo5uPDx6CHI",
+                "link" => "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
                 "banner" => "https://letraslibres.com/wp-content/uploads/2020/08/loro%20del%20rin.jpg",
                 'project_id' => 1,
               'created_at' => "2022-01-01",
